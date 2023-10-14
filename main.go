1bea99eca6bbc952b2143f939715767d177e74b0
Irori235 <Irori235@gmail.com>
2023-10-14 12:34:11 +0900

    🔧 Replace echo to gin

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"log"
+
 	"github.com/Irori235/system-design-2023-v2/internal/handler"
 	"github.com/Irori235/system-design-2023-v2/internal/migration"
 	"github.com/Irori235/system-design-2023-v2/internal/pkg/config"
 	"github.com/Irori235/system-design-2023-v2/internal/repository"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/middleware"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -15,20 +16,16 @@ func main() {
 	// setup gin
 	r := gin.Default()
 
-	// middlewares
-	r.Use(middleware.RequestID())
-	r.Use(middleware.Recovery())
-
 	// connect to database
 	db, err := sqlx.Connect("mysql", config.MySQL().FormatDSN())
 	if err != nil {
-		e.Logger.Fatal(err)
+		log.Fatal(err)
 	}
 	defer db.Close()
 
 	// migrate tables
 	if err := migration.MigrateTables(db.DB); err != nil {
-		e.Logger.Fatal(err)
+		log.Fatal(err)
 	}
 
 	// setup repository
@@ -36,8 +33,8 @@ func main() {
 
 	// setup routes
 	h := handler.New(repo)
-	v1API := e.Group("/api/v1")
+	v1API := r.Group("/api/v1")
 	h.SetupRoutes(v1API)
 
-	e.Logger.Fatal(e.Start(config.AppAddr()))
+	log.Fatal(r.Run(config.AppAddr()))
 }
